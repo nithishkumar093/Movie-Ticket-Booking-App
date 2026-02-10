2ce76b222febe53346282c3ccbccbd87e37c0078
nithishkumar093 <nithishsanju52@gmail.com>
2026-02-10 11:55:31 +0530

    code updated

--- Backend/server.js
+++ Backend/server.js
@@ -5,8 +5,8 @@ import connectDB from "./configs/db.js";
 import { clerkMiddleware } from '@clerk/express'
 import { serve } from "inngest/express";
 import { inngest, functions } from "./inngest/index.js"
-const app=express();
-const port=process.env.PORT || 3000;
+const app = express();
+const port = process.env.PORT || 3000;
 
 await connectDB();
 
@@ -16,7 +16,9 @@ app.use(cors());
 app.use(clerkMiddleware())
 
 //API Routes
-app.get('/',(req,res)=>res.send('Server is Live!'))
+app.get('/', (req, res) => res.send('Server is Live!'))
 app.use('/api/inngest', serve({ client: inngest, functions }))
 
-app.listen(port,()=>console.log(`Server listening at http://localhost:${port}`))
+app.listen(port, () => console.log(`Server listening at http://localhost:${port}`))
+
+export default app;
