c2e775ca4d064f4798aec47a72ec12f0b0d0806b
nithishkumar093 <nithishsanju52@gmail.com>
2026-02-11 13:42:28 +0530

    server updated

--- Backend/server.js
+++ Backend/server.js
@@ -17,8 +17,7 @@ app.use(clerkMiddleware())
 
 //API Routes
 app.get('/', (req, res) => res.send('Server is Live!'))
-app.use('/api/inngest', serve(inngest, functions));
-
+app.use('/api/inngest', serve({ client: inngest, functions }))
 
 
 app.listen(port, () => console.log(`Server listening at http://localhost:${port}`))
